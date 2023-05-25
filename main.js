9a0388f66ddaf25d692e7385dac721cb744ab960
IEatSushi <2klaus.4peter@gmail.com>
2023-05-25 17:32:52 +0200

    add test backrgound  texture

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 
-const renderer = new THREE.WebGLRenderer({"antialias": true});
+const renderer = new THREE.WebGLRenderer({
+	antialias: true,
+	preserveDrawingBuffer: true
+});
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 10;
+camera.position.z = 15;
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
@@ -24,14 +28,30 @@ target.position.set(-1, 0, -3);
 const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
 directionalLight.target = target;
 
+const texture = new THREE.TextureLoader().load("./public/backdrop.png");
+texture.wrapS = THREE.ClampToEdgeWrapping;
+texture.wrapT = THREE.ClampToEdgeWrapping;
+texture.encoding = THREE.sRGBEncoding;
+
+const planeGeometry = new THREE.PlaneGeometry(.5 * 16, .5 * 9);
+const planeMaterial = new THREE.MeshBasicMaterial({
+	side: THREE.DoubleSide,
+	map: texture
+});
+
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+
 scene.add(cube);
 scene.add(ambientLight);
 scene.add(target);
 scene.add(directionalLight);
+scene.add(plane);
+
 
 function animate() {
 	requestAnimationFrame(animate);
 	controls.update();
 	renderer.render(scene, camera);
 }
+
 animate();
