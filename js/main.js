8d312b09b15770c68db0a1a42a39350e6836e63e
IEatSushi <2klaus.4peter@gmail.com>
2023-05-25 23:09:01 +0200

    copy mouse rotation logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
-import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
+import {RotationControl} from './RotationControl'
+
+const phi = (1 + Math.sqrt(5)) / 2;
 
 const renderer = new THREE.WebGLRenderer({
 	antialias: true,
@@ -11,14 +13,16 @@ document.body.appendChild(renderer.domElement);
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 15;
+camera.position.z = 20;
 
-const controls = new OrbitControls(camera, renderer.domElement);
+const controls = new RotationControl(camera, renderer.domElement);
 controls.enableDamping = true;
 
 const geometry = new THREE.IcosahedronGeometry(1, 0);
 const material = new THREE.MeshLambertMaterial({color: 0x515151})
 const cube = new THREE.Mesh(geometry, material);
+cube.rotation.z += Math.atan(1 / phi);
+cube.rotation.y += Math.atan(-0.5 / phi);
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
 
@@ -33,14 +37,14 @@ texture.wrapS = THREE.ClampToEdgeWrapping;
 texture.wrapT = THREE.ClampToEdgeWrapping;
 texture.encoding = THREE.sRGBEncoding;
 
-const planeGeometry = new THREE.PlaneGeometry(.5 * 16, .5 * 9);
+const planeGeometry = new THREE.PlaneGeometry(16, 9);
 const planeMaterial = new THREE.MeshBasicMaterial({
 	side: THREE.DoubleSide,
 	map: texture
 });
 
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-
+plane.position.set(0, 0, -5);
 scene.add(cube);
 scene.add(ambientLight);
 scene.add(target);
@@ -50,6 +54,7 @@ scene.add(plane);
 
 function animate() {
 	requestAnimationFrame(animate);
+
 	controls.update();
 	renderer.render(scene, camera);
 }
